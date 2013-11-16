57c5343f391eb4f889ef66562170732332a32a1e
dzhang50 <dzhang50@d70064af-3dad-418b-850f-cde9b4438f0b>
2013-11-16 12:00:32 +0000

    Updated dictionary script
    git-svn-id: https://subversion.assembla.com/svn/relevantxkcd@12 d70064af-3dad-418b-850f-cde9b4438f0b

--- scripts/dict.py
+++ scripts/dict.py
@@ -5,11 +5,7 @@ import glob, os, sys, re
 raw = re.compile("raw\_");
 
 err = open("dict_log", "w");
-dicts = [];
-dict1 = {};
-dict2 = {};
-dicts.append(dict1);
-dicts.append(dict2);
+globalDict = {};
 for root, dirs, files in os.walk("strip"):
     files.sort();
     
@@ -26,16 +22,61 @@ for root, dirs, files in os.walk("strip"):
             print lines;
             sys.exit(1);
         
-        myDict = {};
-        cnt = 0;
         for line in lines:
             words = line.split();
             for word in words:
                 # Update local dictionary
-                if(myDict.has_key(word)):
-                    myDict[word] = myDict[word] + 1;
+                if(globalDict.has_key(word)):
+                    globalDict[word] = globalDict[word] + 1;
                 else:
-                    myDict[word] = 1;
+                    globalDict[word] = 1;
+
+sortedGlobalDict = [(k, globalDict[k]) for k in sorted(globalDict, key=globalDict.get, reverse=True)];
+globalDictList = [];
+out = open("globalDict", "w");
+for entry in sortedGlobalDict:
+    #print entry;
+    out.write(entry[0]+" "+str(entry[1])+"\n");
+    globalDictList.append(entry[0]);
+out.close();
+
+
+#--------------------------------------
+
+dicts = [];
+dict1 = {};
+dict2 = {};
+dicts.append(dict1);
+dicts.append(dict2);
+
+for root, dirs, files in os.walk("strip"):
+    files.sort();
+    
+    for fi in files:
+        filename = os.path.join(root, fi);
+        print "Processing " + filename;
+        f = open(filename, "r");
+        lines = [];
+        for line in f:
+            lines.append(line);
+        
+        if len(lines) != 2:
+            print "ERROR: LINES NOT 2";
+            print lines;
+            sys.exit(1);
+        
+        myDicts = [];
+        myDicts.append({});
+        myDicts.append({});
+        cnt = 0;
+        for line in lines:
+            words = line.split();
+            for word in words:
+                # Update local explanation dictionary
+                if(myDicts[cnt].has_key(word)):
+                    myDicts[cnt][word] = myDicts[cnt][word] + 1;
+                else:
+                    myDicts[cnt][word] = 1;
                 
                 # Update global explanation and transcript 
                 # dictionaries for weighting
@@ -46,25 +87,57 @@ for root, dirs, files in os.walk("strip"):
             cnt = cnt + 1;
 
         # Write Output file
-        name = raw.sub("", fi);
-        out = open("dicts/"+name, "w");
-        sortedDict = [(k, myDict[k]) for k in sorted(myDict, key=myDict.get, reverse=True)];
-        for entry in sortedDict:
-            out.write(entry[0]+" "+str(entry[1])+"\n");
-        out.close();
+        for i in range(2):
+            name = raw.sub("", fi);
+            f = "";
+            if i == 0:
+                f = "dicts/explain_"+name;
+            elif i == 1:
+                f = "dicts/transcript_"+name;
+            out = open(f, "w");
+            sortedDict = [(k, myDicts[i][k]) for k in sorted(myDicts[i], key=myDicts[i].get, reverse=True)];
+            for entry in sortedDict:
+                # Find dimension corresponding to word
+                idx = globalDictList.index(entry[0]);
+                if(idx == -1):
+                    print "ERROR: NOT FOUND IN GLOBALDICTLIST";
+                    print entry[0];
+                    sys.exit(1);
+                #print entry[0]+" found at "+str(idx);
+                out.write(str(idx)+" "+entry[0]+" "+str(entry[1])+"\n");
+            out.close();
+
+
 
 # Create explanation dictionary
+print "Creating global explanation dictionary";
 sortedDict = [(k, dicts[0][k]) for k in sorted(dicts[0], key=dicts[0].get, reverse=True)];
 out = open("explainDict", "w");
 for entry in sortedDict:
-    print entry;
-    out.write(entry[0]+" "+str(entry[1])+"\n");
+    #print entry;
+    # Find dimension corresponding to word
+    idx = globalDictList.index(entry[0]);
+    if(idx == -1):
+        print "ERROR: NOT FOUND IN GLOBALDICTLIST";
+        print entry[0];
+        sys.exit(1);
+    #print entry[0]+" found at "+str(idx);
+    out.write(str(idx)+" "+entry[0]+" "+str(entry[1])+"\n");
 out.close();
 
-# Create explanation dictionary
+# Create transcript dictionary
+print "Creating transcript dictionary";
 sortedDict = [(k, dicts[1][k]) for k in sorted(dicts[1], key=dicts[1].get, reverse=True)];
 out = open("transcriptDict", "w");
 for entry in sortedDict:
-    print entry;
-    out.write(entry[0]+" "+str(entry[1])+"\n");
+    #print entry;
+    # Find dimension corresponding to word
+    idx = globalDictList.index(entry[0]);
+    if(idx == -1):
+        print "ERROR: NOT FOUND IN GLOBALDICTLIST";
+        print entry[0];
+        sys.exit(1);
+    #print entry[0]+" found at "+str(idx);
+    #out.write(str(idx)+" "+entry[0]+" "+str(entry[1])+"\n");
+    out.write(str(idx)+" "+entry[0]+" "+str(entry[1])+"\n");
 out.close();
